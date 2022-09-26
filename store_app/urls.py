55595867e34bf2c461d307a0ddbd65af084ee3f1
Roman Tarassenko <tarassenko@me.com>
2022-09-26 18:00:12 +0300

    Создан функционал добавления новых продуктов через форму на сайте.

--- store_app/urls.py
+++ store_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from store_app.views import index_view, product_view, categories_add_view
+from store_app.views import index_view, product_view, categories_add_view, add_product_view
 
 urlpatterns = [
     path('', index_view, name='home'),
     path('products/', index_view),
     path('products/<int:pk>', product_view, name='product_detailed'),
-    path('categories/add', categories_add_view, name='add_new_category')
+    path('categories/add', categories_add_view, name='add_new_category'),
+    path('products/add/', add_product_view, name='add_new_product')
 ]
--- store_app/views.py
+++ store_app/views.py
@@ -24,3 +24,21 @@ def categories_add_view(request):
                          }
         Category.objects.create(**category_data)
         return redirect('home')
+
+
+def add_product_view(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        context = {'categories': categories}
+        return render(request, 'product_add.html', context=context)
+    elif request.method == 'POST':
+        category_pk = int(request.POST.get('category'))
+        product_data = {
+            'name': request.POST.get('name'),
+            'price': request.POST.get('price'),
+            'description': request.POST.get('description'),
+            'category': Category.objects.get(pk=category_pk),
+            'picture_link': request.POST.get('picture_link')
+        }
+        new_product = Product.objects.create(**product_data)
+        return redirect('product_detailed', pk=new_product.pk)
